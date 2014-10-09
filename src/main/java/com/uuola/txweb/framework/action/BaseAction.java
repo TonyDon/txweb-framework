09ff89689054d365bf2e9dd4e39fb6048c722af2
TonyDon <hongdeviltxd@163.com>
2014-10-09 14:56:53 +0800

    update BaseAction  将查询方法移到BaseAction

--- src/main/java/com/uuola/txweb/framework/action/BaseAction.java
+++ src/main/java/com/uuola/txweb/framework/action/BaseAction.java
@@ -26,8 +26,11 @@ import com.uuola.commons.constant.CST_CHAR;
 import com.uuola.commons.constant.HTTP_STATUS_CODE;
 import com.uuola.commons.exception.BusinessException;
 import com.uuola.commons.exception.BusinessExceptionMessageProvider;
+import com.uuola.txweb.framework.action.methods.QueryCallbackHandler;
 import com.uuola.txweb.framework.action.methods.UpdateCallbackHandler;
+import com.uuola.txweb.framework.dto.PageDTO;
 import com.uuola.txweb.framework.dto.ValidateDTO;
+import com.uuola.txweb.framework.query.BaseQuery;
 import com.uuola.txweb.framework.utils.TxAssert;
 import com.uuola.txweb.framework.utils.ValidateUtil;
 
@@ -48,6 +51,9 @@ public abstract class BaseAction {
     protected static final String ERRORS_ATTR = "errors";
 
     protected static final String UPDATE_RESULT_ATTR = "updateResult";
+    
+    // 页面使用PageDTO的属性名称 如: page.datas , page.total
+    protected static final String QUERY_PAGE_ATTR = "page";
 
     /**
      * 根据包路径和Action类构建视图名路径，如： com.uuola.txcms.portal.user.action.UserInfoAction
@@ -187,6 +193,44 @@ public abstract class BaseAction {
             try {
                 T result = handler.doUpdate(clientDTO);
                 model.addObject(UPDATE_RESULT_ATTR, result);
+            } catch (BusinessException be) {
+                errors.add(BusinessExceptionMessageProvider.getMessage(be));
+                log.error("", be);
+            } catch (Exception e) {
+                errors.add(ExceptionUtils.getFullStackTrace(e));
+                log.error("", e);
+            }
+        }
+        if (!errors.isEmpty()) {
+            model.addObject(ERRORS_ATTR, errors);
+            webRequest.getResponse().setStatus(HTTP_STATUS_CODE.SC_BZ_ERROR);
+        }
+        return model;
+    }
+    
+    
+    /**
+     * 封装查询过程
+     * 
+     * @param query 查询条件
+     * @param handler 查询处理外部回调执行者
+     * @param webRequest 当前Request
+     * @return
+     */
+    protected ModelAndView executeQuery(ServletWebRequest webRequest, BaseQuery query, QueryCallbackHandler handler) {
+        ModelAndView model = new ModelAndView();
+        List<String> errors = new ArrayList<String>();
+        if (query.isNeedValid() && !query.validatePass()) {
+            errors.addAll(getErrors(query));
+        } else {
+            try {
+                // 执行查询条件过滤方法，如非法值过滤，默认条件设置, 值转换等
+                query.filter();
+                // 执行分页查询记录行计算
+                query.calcCurrRowIndex();
+                // 查询返回PageDTO对象
+                PageDTO pageDTO = handler.doQuery(query);
+                model.addObject(QUERY_PAGE_ATTR, pageDTO);
             } catch (BusinessException be) {
                 errors.add(BusinessExceptionMessageProvider.getMessage(be));
                 log.error("", be);
